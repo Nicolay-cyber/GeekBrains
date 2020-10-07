a1e6aa385ba433445eadb7bcd6dbb0305617d071
st.nico <157130401678@mail.ru>
2020-10-07 11:39:25 +0300

    Выполненная домашка к вебинару от 05.10.2020

--- src/ru/geekbrains/lesson4/Lesson4.java
+++ src/ru/geekbrains/lesson4/Lesson4.java
@@ -12,17 +12,17 @@ public class Lesson4 {
     static char AI_PIECE = DOT_EMPTY;
     static char PLAYER_PIECE = DOT_EMPTY;
     static boolean IS_PLAYER_FIRST;
-    static boolean IS_GAME_OVER;
+    static int SCORE = 0;
 
-
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         choosePiece();
         setSize();
         setDotsToWin();
         createMap();
         chooseFirstPlayer();
         printMap();
-        while (!IS_GAME_OVER){
+        while (true){
             if (IS_PLAYER_FIRST){
                 playerTurn();
                 if(hasWin(PLAYER_PIECE)){
@@ -37,11 +37,11 @@ public class Lesson4 {
                 }
             }
             aiTurn();
-                if(hasWin(AI_PIECE)){
-                    printMap();
-                    System.out.println("\nComputer is win");
-                    break;
-                }
+            if(hasWin(AI_PIECE)){
+                printMap();
+                System.out.println("\nComputer is win");
+                break;
+            }
             if(hasDraw()){
                 printMap();
                 System.out.println("\nIt's draw!");
@@ -50,8 +50,8 @@ public class Lesson4 {
             printMap();
         }
     }
-
-    static void choosePiece(){
+    static void choosePiece()
+    {
         System.out.println("\nChoose the game piece\n\nPress 1 for X\nPress 2 for O\n");
         switch (getNumberFromConsoleOnlyBetween(1,2)){
             case 1:
@@ -65,16 +65,18 @@ public class Lesson4 {
         }
         System.out.println("Your piece is " + PLAYER_PIECE);
     }
-    static void setSize(){
+    static void setSize()
+    {
         System.out.println("\nSet the size of the map from 3 to 9");
         SIZE = getNumberFromConsoleOnlyBetween(3, 9);
     }
-    static void setDotsToWin(){
+    static void setDotsToWin()
+    {
         System.out.println("\nSet the count dots to win from 2 to " + SIZE);
         DOTS_TO_WIN = getNumberFromConsoleOnlyBetween(2, SIZE);
-
     }
-    static int getNumberFromConsoleOnlyBetween(int from, int to){
+    static int getNumberFromConsoleOnlyBetween(int from, int to)
+    {
         do{
             int inputNumber = getNumberFromConsole(from, to);
             if (inputNumber >= from && inputNumber <= to){
@@ -84,7 +86,8 @@ public class Lesson4 {
         }
         while (true);
     }
-    static int getNumberFromConsole(int from, int to){
+    static int getNumberFromConsole(int from, int to)
+    {
         Scanner scanner = new Scanner(System.in);
         do
         {
@@ -118,7 +121,8 @@ public class Lesson4 {
             Arrays.fill(map[i], DOT_EMPTY);
         }
     }
-    static void printMap(){
+    static void printMap()
+    {
         System.out.println();
         for (int i = 0; i < map.length + 1; i++)
         {
@@ -149,14 +153,7 @@ public class Lesson4 {
         {
             return false;
         }
-        else if (map[y][x] == DOT_EMPTY)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        else return map[y][x] == DOT_EMPTY;
     }
     static void playerTurn()
     {
@@ -172,7 +169,8 @@ public class Lesson4 {
         while (!(isCellEmpty(x, y)));
         map[y][x] = PLAYER_PIECE;
     }
-    static void aiTurn(){
+    static void aiTurn()
+    {
         Random random = new Random();
         int x;
         int y;
@@ -184,137 +182,151 @@ public class Lesson4 {
         while (!isCellEmpty(x,y));
         System.out.println("\nThe computer moves to the point: " + (x + 1) + " " + (y + 1));
         map[y][x] = AI_PIECE;
-            IS_PLAYER_FIRST = true;
+        IS_PLAYER_FIRST = true;
     }
-
     static boolean hasWin(char symbol)
     {
-        // по строкам
-        int count = 0;
-        for(int i = 0; i < SIZE; i++)
+        return checkingOfTheStrings(symbol) ||
+                checkingOfTheColumns(symbol) ||
+                checkingOfThePrimaryDiagonals(symbol) ||
+                checkingOfTheSecondaryDiagonals(symbol);
+    }
+    static boolean checkingOfTheSecondaryDiagonals(char symbol)
+    {
+        // по побочным диагоналям
+        SCORE = 0;
+        for (int i = 0; i < (SIZE * 2) - 1; i++)
         {
-            for (int j = 0; j < SIZE; j++)
+            for(int j = 0; j < SIZE - Math.abs(SIZE - (i + 1)); j ++)
             {
-                if(count == DOTS_TO_WIN)
+                if(SCORE == DOTS_TO_WIN)
                 {
                     return true;
                 }
-                if((map[i][j] != symbol && count > 0))
+                if(i > (SIZE - 1))
                 {
-                    count = 0;
+                    if(map[i-(SIZE - 1) + j][(SIZE - 1) - j] != symbol && SCORE > 0){
+                        SCORE = 0;
+                    }
+                    else if(map[i-(SIZE - 1) + j][(SIZE - 1) - j] == symbol)
+                    {
+                        SCORE++;
+                    }
                 }
-                else if(map[i][j] == symbol)
+                else
                 {
-                    count++;
+                    if(map[j][i-j] != symbol && SCORE > 0)
+                    {
+                        SCORE = 0;
+                    }
+                    else if(map[j][i-j] == symbol)
+                    {
+                        SCORE++;
+                    }
                 }
-                if(j == SIZE - 1 && count < DOTS_TO_WIN){
-                    count = 0;
+                if(j == (SIZE - Math.abs(SIZE - (i + 1))) - 1 && SCORE < DOTS_TO_WIN){
+
+                    SCORE = 0;
                 }
             }
 
         }
-        // по столбцам
-        count = 0;
+
+        return false;
+    }
+    static boolean checkingOfThePrimaryDiagonals(char symbol)
+    {
+        SCORE = 0;
+        for (int i = 0; i < (SIZE * 2) - 1; i++)
+        {
+            for(int j = 0; j < SIZE - Math.abs(SIZE - (i + 1)); j ++)
+            {
+                if(SCORE == DOTS_TO_WIN)
+                {
+                    return true;
+                }
+                if(i > (SIZE - 1))
+                {
+                    if(map[j][j + i - (SIZE -1)] != symbol && SCORE > 0){
+                        SCORE = 0;
+                    }
+                    else if(map[j][j + i - (SIZE -1)] == symbol)
+                    {
+                        SCORE++;
+                    }
+                }
+                else
+                {
+                    if(map[(SIZE - 1) - i + j][j] != symbol && SCORE > 0)
+                    {
+                        SCORE = 0;
+                    }
+                    else if(map[(SIZE - 1) - i + j][j] == symbol)
+                    {
+                        SCORE++;
+                    }
+
+                }
+                if(j == (SIZE - Math.abs(SIZE - (i + 1))) - 1 && SCORE < DOTS_TO_WIN){
+                    SCORE = 0;
+                }
+            }
+
+        }
+
+        return false;
+    }
+    static boolean checkingOfTheColumns(char symbol)
+    {
+        SCORE = 0;
         for(int i = 0; i < map.length; i++)
         {
             for(int j = 0; j < map.length; j++)
             {
-                if(count == DOTS_TO_WIN)
+                if(SCORE == DOTS_TO_WIN)
                 {
                     return true;
                 }
-                if(map[j][i] != symbol && count > 0)
+                if(map[j][i] != symbol && SCORE > 0)
                 {
-                    count = 0;
+                    SCORE = 0;
                 }
                 else if(map[j][i] == symbol)
                 {
-                    count++;
+                    SCORE++;
                 }
-                if(j == SIZE - 1 && count < DOTS_TO_WIN){
-                    count = 0;
+                if(j == SIZE - 1 && SCORE < DOTS_TO_WIN){
+                    SCORE = 0;
                 }
             }
         }
-        // по побочным диагоналям
-        count = 0;
-        for (int i = 0; i < (SIZE * 2) - 1; i++)
-        {
-            for(int j = 0; j < SIZE - Math.abs(SIZE - (i + 1)); j ++)
-            {
-                if(count == DOTS_TO_WIN)
-                {
-                    return true;
-                }
-                if(i > (SIZE - 1))
-                {
-                    if(map[i-(SIZE - 1) + j][(SIZE - 1) - j] != symbol && count > 0){
-                        count = 0;
-                    }
-                    else if(map[i-(SIZE - 1) + j][(SIZE - 1) - j] == symbol)
-                    {
-                        count++;
-                    }
-                }
-                else
-                {
-                    if(map[j][i-j] != symbol && count > 0)
-                    {
-                        count = 0;
-                    }
-                    else if(map[j][i-j] == symbol)
-                    {
-                        count++;
-                    }
-                }
-                if(j == (SIZE - Math.abs(SIZE - (i + 1))) - 1 && count < DOTS_TO_WIN){
-                    count = 0;
-                }
-            }
-
-        }
-        //по главным диагоналям
-        count = 0;
-        for (int i = 0; i < (SIZE * 2) - 1; i++)
-        {
-            for(int j = 0; j < SIZE - Math.abs(SIZE - (i + 1)); j ++)
-            {
-                if(count == DOTS_TO_WIN)
-                {
-                    return true;
-                }
-                if(i > (SIZE - 1))
-                {
-                    if(map[j][j + i - (SIZE -1)] != symbol && count > 0){
-                        count = 0;
-                    }
-                    else if(map[j][j + i - (SIZE -1)] == symbol)
-                    {
-                        count++;
-                    }
-                }
-                else
-                {
-                    if(map[(SIZE - 1) - i + j][j] != symbol && count > 0)
-                    {
-                        count = 0;
-                    }
-                    else if(map[(SIZE - 1) - i + j][j] == symbol)
-                    {
-                        count++;
-                    }
-
-                }
-                if(j == (SIZE - Math.abs(SIZE - (i + 1))) - 1 && count < DOTS_TO_WIN){
-                    count = 0;
-                }
-            }
-
-        }
-
-
-
         return false;
+    }
+    static boolean checkingOfTheStrings(char symbol)
+    {
+        for(int i = 0; i < SIZE; i++)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
+                if(SCORE == DOTS_TO_WIN)
+                {
+                    return true;
+                }
+                if((map[i][j] != symbol && SCORE > 0))
+                {
+                    SCORE = 0;
+                }
+                else if(map[i][j] == symbol)
+                {
+                    SCORE++;
+                }
+                if(j == SIZE - 1 && SCORE < DOTS_TO_WIN){
+                    SCORE = 0;
+                }
+            }
+        }
+        return false;
+
     }
     static boolean hasDraw()
     {
